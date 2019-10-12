66bedb6daab0c0f77330f48cb3ebc6efad3dacd4
Dylan Parrish <30538349+dparrish55@users.noreply.github.com>
2019-10-12 18:48:53 -0500

    Added AcePilot, BountyHunter, Icon, and Mercenary

---
+++ Themes/Icon.py
@@ -0,0 +1,23 @@
+class Icon:
+    def __init__(self):
+        self.ability_improvement = {"CHA": 1}
+        self.traits = {
+            1: {"Icon Knowledge":
+                "Choose a Profession skill. You are hooked deeply into the "
+                "culture of your iconic profession.  When attempting a "
+                "Profession or Culture check to recall knowledge about other "
+                "icons of your profession or details about your profession's "
+                "cultural aspects, increase the DC by 5.  You gain a +1 bonus "
+                "to checks with your chosen Profession skill."
+                "Culture also becomes a class "
+                "skill for you, though if it a class skill from the class you "
+                "take at 1st level, you instead gain a +1 bonus to your "
+                "Culture checks. In addition, you gain an ability adjustment "
+                "of +1 to Charisma at character creation."},
+            6: {"Celebrity":
+                "Future Implement"},
+            12: {"MegaCelebrity":
+                "Future Implement"},
+            18: {"Master Icon":
+                "Future Implement"}
+        }
---
+++ Themes/Mercenary.py
@@ -0,0 +1,24 @@
+class Mercenary:
+    def __init__(self):
+        self.ability_improvement = {"STR": 1}
+        self.traits = {
+            1: {"Mercenary Knowledge":
+                "You are knowledgeable about the military, from rival "
+                "mercenary groups to standard military procedures to planetary "
+                "armed forces, and you can draw upon this fount of information "
+                "to aid your adventurous pursuits.  Reduce the DC of Culture "
+                "checks and Profession (mercenary) checks to recall knowledge "
+                "about hierarchies, practices, personnel, and so on in the "
+                "military by 5."
+                "Athletics is a class skill for you, though if it is a class "
+                "skill from the class you take at 1st level, you instead gain "
+                "a +1 bonus to Athletics checks.  In addition, you gain an "
+                "ability adjustment of +1 to Strength at character "
+                "creation."},
+            6: {"Grunt":
+                "Future Implement"},
+            12: {"Squad Leader":
+                "Future Implement"},
+            18: {"Commander":
+                "Future Implement"}
+        }
