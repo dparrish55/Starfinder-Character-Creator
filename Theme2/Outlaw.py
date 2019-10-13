96c1bc2dd0cc942af51b4467bc0d6e3bf1cdb873
Dylan Parrish <30538349+dparrish55@users.noreply.github.com>
2019-10-12 19:24:10 -0500

    Directory Theme2 to replace erroring Theme directory....

---
+++ Theme2/Outlaw.py
@@ -0,0 +1,21 @@
+class Outlaw:
+    def __init__(self):
+        self.ability_improvement = {"DEX": 1}
+        self.traits = {
+            1: {"Outlaw Knowledge":
+                "You are well connected to shadowy secrets and back-alley "
+                "deals, and you both know about key players and have handy "
+                "skills of your own.  Reduce the DC of Culture checks to "
+                "recall knowledge about the criminal underworld by 5. "
+                "Sleight of Hand is a class skill for you, though if it is a "
+                "class skill from the class you take at 1st level, you instead "
+                "gain a +1 bonus to Sleight of Hand checks.  In addition, you "
+                "gain an ability adjustment of +1 to Dexterity at character "
+                "creation."},
+            6: {"Legal Corruption":
+                "Future Implement"},
+            12: {"Black Market Connections":
+                "Future Implement"},
+            18: {"Master Outlaw":
+                "Future Implement"}
+        }
